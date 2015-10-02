303ec9a6950bab47af2235ea00feb68c93665964
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2015-10-03 04:47:12 +0800

    Able to send and receive file

--- ReliableFTP/SimpleUDPReceiver.java
+++ ReliableFTP/SimpleUDPReceiver.java
@@ -3,16 +3,22 @@
 
 // All numbers below are represented in bytes
 // First packet design:
-// [8-Checksum][255-DestinationFilepath]
+// [8-Checksum]
+// [255-DestinationFilepath]
+// [8-Filesize]
 
 import java.net.*;
 import java.util.*;
 import java.nio.*;
 import java.util.zip.*;
+import java.io.*;
 
 public class SimpleUDPReceiver {
 	// Filename max 255 characters (UTF-8 is 1 byte per character, hence 255 bytes)
 	public static int filepathByteSize = 255;
+	public static int headerSize = 8;
+	public static int maxPacketSize = 1400;
+	public static int maxDataSize = maxPacketSize-headerSize;
 
 	public static void main(String[] args) throws Exception 
 	{
@@ -22,10 +28,61 @@ public class SimpleUDPReceiver {
 		}
 		int port = Integer.parseInt(args[0]);
 		DatagramSocket sk = new DatagramSocket(port);
-		byte[] data = new byte[1400];
+		byte[] data = new byte[maxPacketSize];
 		DatagramPacket pkt = new DatagramPacket(data, data.length);
 		ByteBuffer b = ByteBuffer.wrap(data);
 		CRC32 crc = new CRC32();
+
+		String destFilename = "";
+		long destFilesize = 0;
+
+		long totalBytesReceived = 0;
+
+		// Receive first packet with the destination filename
+		if (true) {
+			pkt.setLength(data.length);
+			sk.receive(pkt);
+			if (pkt.getLength() < 8)
+			{
+				System.out.println("Pkt too short");
+				return;
+			}
+			b.rewind();
+			long chksum = b.getLong();
+			crc.reset();
+			crc.update(data, 8, pkt.getLength()-8);
+			// Debug output
+			// System.out.println("Received CRC:" + crc.getValue() + " Data:" + bytesToHex(data, pkt.getLength()));
+			if (crc.getValue() != chksum)
+			{
+				System.out.println("Pkt corrupt");
+			}
+			else
+			{
+				// Get filename
+				byte[] byteDest = new byte[filepathByteSize];
+				b.get(byteDest, 0, filepathByteSize);
+				destFilename = new String(byteDest).trim();
+				destFilesize = b.getLong();
+
+				System.out.println("Pkt " + destFilename);
+				
+				// DatagramPacket ack = new DatagramPacket(new byte[0], 0, 0, pkt.getSocketAddress());
+				// sk.send(ack);
+			}	
+		}
+
+		BufferedOutputStream bos;
+		try {
+			bos = new BufferedOutputStream(new FileOutputStream(destFilename));
+		}
+		catch (Exception e) {
+			System.out.println("Failed in writing file!");
+			return;
+		}
+
+		int counter = 1;
+		
 		while(true)
 		{
 			pkt.setLength(data.length);
@@ -47,18 +104,25 @@ public class SimpleUDPReceiver {
 			}
 			else
 			{
-				// Get filename
-				byte[] byteDest = new byte[filepathByteSize];
-				b.get(byteDest, 0, filepathByteSize);
-				String destFilename = new String(byteDest).trim();
+				byte[] dataRcvd = new byte[maxDataSize];
+				b.get(dataRcvd);
+				bos.write(dataRcvd);
+				bos.flush();
 
-				System.out.println("Pkt " + destFilename);
-				
-				DatagramPacket ack = new DatagramPacket(new byte[0], 0, 0,
-						pkt.getSocketAddress());
-				sk.send(ack);
-			}	
+				totalBytesReceived += dataRcvd.length;
+
+				System.out.println("Pkt received " + counter++ + " data size " + dataRcvd.length);
+				// DatagramPacket ack = new DatagramPacket(new byte[0], 0, 0, pkt.getSocketAddress());
+				// sk.send(ack);
+			}
+
+			if (totalBytesReceived >= destFilesize) {
+				System.out.println(totalBytesReceived);
+				bos.close();
+				break;
+			}
 		}
+
 	}
 
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
--- ReliableFTP/SimpleUDPSender.java
+++ ReliableFTP/SimpleUDPSender.java
@@ -3,7 +3,9 @@
 
 // All numbers below are represented in bytes
 // First packet design:
-// [8-Checksum][255-DestinationFilepath]
+// [8-Checksum]
+// [255-DestinationFilepath]
+// [8-Filesize]
 
 import java.net.*;
 import java.util.*;
@@ -11,10 +13,14 @@ import java.nio.*;
 
 import java.nio.charset.*;
 import java.util.zip.*;
+import java.io.*;
 
 public class SimpleUDPSender {
 	// Filename max 255 characters (UTF-8 is 1 byte per character, hence 255 bytes)
 	public static int filepathByteSize = 255;
+	public static int headerSize = 8;
+	public static int maxPacketSize = 1400;
+	public static int maxDataSize = maxPacketSize-headerSize;
 
 	private static byte[] toBytes(char[] chars) {
 		CharBuffer charBuffer = CharBuffer.wrap(chars);
@@ -35,32 +41,57 @@ public class SimpleUDPSender {
 		String sourceFilepath = args[2];
 		byte[] byteArrDestFilepath = toBytes(args[3].toCharArray());
 
-		InetSocketAddress addr = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
+		File sourceFile;
+		BufferedInputStream bis;
 
+		try {
+			sourceFile = new File(sourceFilepath);
+			bis = new BufferedInputStream(new FileInputStream(sourceFile));
+		} catch (Exception e) {
+			System.out.println("File not found");
+			return;
+		}
+
+
+		// Obtain Socket Address
+		InetSocketAddress addr = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
 		DatagramSocket sk = new DatagramSocket();
 		DatagramPacket pkt;
-		byte[] data = new byte[1400];
+		byte[] data = new byte[maxPacketSize];
+		int len;
 		ByteBuffer b = ByteBuffer.wrap(data);
-
 		CRC32 crc = new CRC32();
 
+		// First packet, send destination address only
 		b.clear();
-		// reserve space for checksum
-		b.putLong(0);
+		b.putLong(0); // reserve space for checksum
 		b.put(byteArrDestFilepath);
 		b.put(new byte[filepathByteSize-byteArrDestFilepath.length]); // append with empty bytes
-		
+		b.putLong(sourceFile.length());
 		crc.reset();
 		crc.update(data, 8, data.length-8);
 		long chksum = crc.getValue();
 		b.rewind();
 		b.putLong(chksum);
-
 		pkt = new DatagramPacket(data, data.length, addr);
-		// Debug output
-		//System.out.println("Sent CRC:" + chksum + " Contents:" + bytesToHex(data));
 		sk.send(pkt);
-		
+
+		int counter = 1;
+		byte[] dataSent = new byte[maxDataSize];
+		while ((len = bis.read(dataSent)) > 0) {
+			// First packet, send destination address only
+			b.clear();
+			b.putLong(0); // reserve space for checksum
+			b.put(dataSent);
+			System.out.println(counter++ +" Sending " + dataSent.length);
+			crc.reset();
+			crc.update(data, 8, data.length-8);
+			chksum = crc.getValue();
+			b.rewind();
+			b.putLong(chksum);
+			pkt = new DatagramPacket(data, data.length, addr);
+			sk.send(pkt);
+		}
 	}
 
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
